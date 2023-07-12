f4ce0e7c61ddb754cd9b5473ba7c22d1d45d02a8
rprudnikov <romanprudon@gmail.com>
2023-07-12 21:19:58 +0300

    fixed game ending

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
+import sys
 
 print("Давай пограємо у гру)")
 name = input("Enter your name: ")
 print(" ")
 print('Гра загадала число від 1 до 50. Тобі треба його відгадати. Гра дає підказки: '
-      'якщо введене користувачем число менше загаданого на 10 (в будь-який бік) - "Холодно", '
-      'якщо на 5-9 - "Тепло", '
+      'якщо введене користувачем число більше загаданого на 11 і більше (в будь-який бік), - "Холодно", '
+      'якщо на 5-10 - "Тепло", '
       'якщо менше 5 - "Гаряче".')
 
 target_number = random.randint(0, 60)
@@ -17,9 +18,11 @@ while True:
     if user_input == target_number:
         print("Ви вгадали!")
         break
-    if abs(result) >= 10:
+    if abs(result) >= 11:
         print("Холодно")
-    if 5 <= abs(result) <= 9:
+    if 5 <= abs(result) <= 10:
         print("Тепло")
     if abs(result) <= 5:
         print("Гаряче")
+input("Натисніть Enter для виходу")
+sys.exit()
